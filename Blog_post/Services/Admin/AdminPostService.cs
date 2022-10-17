e77770c75b43ce65e3160c5de81e27fff58fb939
Asror020 <asrora020@gmail.com>
2022-10-17 14:29:37 +0500

    Service layer has been added.

---
+++ Blog_post/Services/Admin/AdminPostService.cs
@@ -0,0 +1,37 @@
+﻿using Blog_post.Data;
+using Blog_post.Enums;
+using Blog_post.Models;
+using Blog_post.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_post.Services.Admin
+{
+    public class AdminPostService : IAdminPostService
+    {
+        private readonly ApplicationDbContext _context;
+        public AdminPostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public List<Post> GetAll()
+        {
+            var posts = _context.posts.Where(x => x.StatusId != StatusEnum.Draft).OrderByDescending(x => x.CreatedDate).ToList();
+            return posts;
+        }
+        public Post GetById(int id)
+        {
+            var post = _context.posts.FirstOrDefault(x => x.Id == id);
+            return post;
+        }
+        public void Approve(Post post)
+        {
+            post.StatusId = StatusEnum.Approve;
+            _context.SaveChanges();
+        }
+        public void Reject(Post post)
+        {
+            post.StatusId = StatusEnum.Reject;
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Blog_post/Services/User/UserPostService.cs
@@ -0,0 +1,54 @@
+﻿using Blog_post.Data;
+using Blog_post.Enums;
+using Blog_post.Models;
+using Blog_post.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog_post.Services.User
+{
+    public class UserPostService : IUserPostService
+    {
+       private readonly ApplicationDbContext _context;
+        public UserPostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Post GetById(int id)
+        {
+            var post = _context.posts.Find(id);
+            return post;
+        }
+        public void EditPost(Post post)
+        {
+            _context.posts.Update(post);
+            _context.SaveChanges();
+        }
+        public void DeletePost(Post post)
+        {
+            _context.posts.Remove(post);
+            _context.SaveChanges();
+        }
+        public bool PostExists(int id)
+        {
+            return _context.posts.Any(x => x.Id == id);
+        }
+
+        public List<Post> GetByAuothorId(string authorId)
+        {
+            var posts = _context.posts.Where(p => p.AuthorId == authorId).OrderByDescending(x => x.CreatedDate).ToList();
+            return posts;
+        }
+
+        public Post Details(int id)
+        {
+            var post = _context.posts.Include(x => x.Status).Where(x => x.Id == id).FirstOrDefault();
+            return post;
+        }
+
+        public void CreatePost(Post post)
+        {
+            _context.posts.Add(post);
+            _context.SaveChanges();
+        }
+    }
+}
